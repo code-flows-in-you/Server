e0736627521b780699c2ac9d88e92c3513897641
Alicizations <673386754@qq.com>
2019-06-24 23:26:09 +0800

    fix bug

--- Server/coin/models.py
+++ Server/coin/models.py
@@ -18,4 +18,4 @@ class CoinFlow(models.Model):
     Flow = models.IntegerField(default = 1)
 
     def __str__(self):
-        return '%s - %s: [%d]' % (self.Uid.Nickname, self.Aid.Title, self.Flow)
+        return '[%s] %s - %s: [%d]' % (self.Type, self.Uid.Nickname, self.Title, self.Flow)
